a8d09566534e8cdc0719473aa19715d1aee6957c
Zia Khan <ziaukhan@hotmail.com>
2016-02-06 14:15:17 +0500

    completed step 4

---
+++ step04_http_request/app/main.js
@@ -0,0 +1,42 @@
+System.register(['rxjs/Rx'], function(exports_1) {
+    var Rx_1;
+    var test;
+    //import { Subject } from 'rxjs/Subject';
+    function get(url) {
+        return Rx_1.Observable.create(function (observer) {
+            // Make a traditional Ajax request
+            var req = new XMLHttpRequest();
+            req.open('GET', url);
+            req.onload = function () {
+                if (req.status == 200) {
+                    // If the status is 200, meaning there have been no problems,
+                    // Yield the result to listeners and complete the sequence
+                    observer.next(req.response);
+                    observer.complete();
+                }
+                else {
+                    // Otherwise, signal to listeners that there has been an error
+                    observer.error(new Error(req.statusText));
+                }
+            };
+            req.onerror = function () {
+                observer.error(new Error("Unknown Error"));
+            };
+            req.send();
+        });
+    }
+    return {
+        setters:[
+            function (Rx_1_1) {
+                Rx_1 = Rx_1_1;
+            }],
+        execute: function() {
+            // Create an Ajax Observable
+            test = get('/app/contents.json');
+            test.subscribe(function next(next) {
+                console.log(next);
+            });
+        }
+    }
+});
+//# sourceMappingURL=main.js.map
---
+++ step04_http_request/app/main.ts
@@ -0,0 +1,32 @@
+import {Observable, Observer} from 'rxjs/Rx';
+//import { Subject } from 'rxjs/Subject';
+
+function get(url) {
+    return Observable.create(function(observer: Observer<String>) {
+    // Make a traditional Ajax request
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function() {
+        if (req.status == 200) {
+        // If the status is 200, meaning there have been no problems,
+        // Yield the result to listeners and complete the sequence
+            observer.next(req.response);
+            observer.complete();
+        }
+        else {
+        // Otherwise, signal to listeners that there has been an error
+            observer.error(new Error(req.statusText));
+        }
+    };
+    req.onerror = function() {
+        observer.error(new Error("Unknown Error"));
+    };
+    req.send();
+});
+}
+// Create an Ajax Observable
+var test = get('/app/contents.json');
+test.subscribe(function next(next: String){
+    console.log(next);
+});
+
