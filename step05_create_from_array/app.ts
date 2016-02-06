9df7f526a60454d42bfd250c52b4e2bdad1d72be
Zia Khan <ziaukhan@hotmail.com>
2016-02-06 15:21:42 +0500

    added step 5

---
+++ step05_create_from_array/app.ts
@@ -0,0 +1,17 @@
+import Rx = require('rxjs/Rx');
+
+
+Rx.Observable
+.from(['Adrià', 'Jen', 'Sergi'])
+.subscribe(
+    function(x: String) { console.log('Next: ' + x); },
+    function(err) { console.log('Error:', err); },
+    function() { console.log('Completed'); }
+);
+
+
+
+
+
+
+
---
+++ step05_create_from_array/readme.md
@@ -0,0 +1,13 @@
+
+
+This step covers the chapter 1 code on page 13 of the following book using RxJS 5 and TypeScript:
+
+http://www.amazon.com/Reactive-Programming-RxJS-Asynchronous-JavaScript-ebook/dp/B01ATYZTI4/
+
+
+To run:
+
+npm install
+
+npm start
+
